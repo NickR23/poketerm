377df4a335b8ae5d7b91c4346f7b841511fed6e4
nickr23 <nickrich2624@gmail.com>
2019-11-23 21:26:01 -0500

    Switch to snake case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let map = poketerm::Pokedex::lists::get_all_pokemon().unwrap();
+    let map = poketerm::pokedex::lists::get_all_pokemon().unwrap();
     for (name, url) in &map {
         println!("{}:{}", name, url);
     }
--- src/pokedex/lists.rs
+++ src/pokedex/lists.rs
@@ -1,4 +1,4 @@
-use crate::Api_Tools::api_tools;
+use crate::api_tools::tools;
 use std::process;
 use std::error;
 use std::collections::HashMap;
@@ -14,18 +14,18 @@ fn populate_map(page: &serde_json::Value, map: &mut HashMap<String,String>) {
 }
 
 fn get_page(url: &str) -> serde_json::Result<serde_json::Value> {
-  let data = api_tools::curl_url(&url).unwrap_or_else( |err| {
+  let data = tools::curl_url(&url).unwrap_or_else( |err| {
       println!("Error {}", err);
       process::exit(1);
     });
-  let response = api_tools::parse_response(&data).unwrap();
+  let response = tools::parse_response(&data).unwrap();
   Ok(response)
 }
 
 ///Returns a HashMap<String,String> of all of the pokemon and their urls
 ///# Example
 ///```
-///let map = poketerm::Pokedex::lists::get_all_pokemon().unwrap();
+///let map = poketerm::pokedex::lists::get_all_pokemon().unwrap();
 ///```
 pub fn get_all_pokemon() -> Result<HashMap<String,String>, Box<dyn error::Error>>{ 
   let mut pokemon_map:HashMap<String, String> = HashMap::new();
@@ -35,7 +35,6 @@ pub fn get_all_pokemon() -> Result<HashMap<String,String>, Box<dyn error::Error>
   let num_pokemon_i64 = data["count"].as_i64().unwrap();
   let num_pokemon: usize = (num_pokemon_i64 as usize).try_into().unwrap();
   
-  println!("{:?}", data);
   while num_pokemon != pokemon_map.len() {
     populate_map(&data, &mut pokemon_map);
     if !data["next"].is_null(){
