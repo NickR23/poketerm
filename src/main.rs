898513c04ae8d396810433c850408de0f236d88e
nickr23 <nickrich2624@gmail.com>
2019-11-23 21:49:48 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use poketerm::ui::driver;
+
 fn main() {
+<<<<<<< Updated upstream
     let map = poketerm::Pokedex::lists::get_all_pokemon().unwrap();
     for (name, url) in &map {
         println!("{}:{}", name, url);
     }
+=======
+  driver::draw_ui().unwrap();
+>>>>>>> Stashed changes
 }
