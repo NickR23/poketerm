5963699b92bf78b2e5656e9296c4121a3a55adc0
nickr23 <nickrich2624@gmail.com>
2019-11-24 19:07:22 -0500

    Added loading notification

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,11 +1,12 @@
 use std::io;
 use std::collections::HashMap;
+use futures::executor::block_on;
 use tui::Terminal;
 use termion::event::Key;
 use tui::backend::TermionBackend;
 use termion::raw::IntoRawMode;
 use tui::widgets::{Widget, Block, Borders, Text, Paragraph, SelectableList};
-use tui::layout::{Layout, Constraint, Direction, Corner};
+use tui::layout::{Layout, Constraint, Direction};
 use termion::screen::AlternateScreen;
 use termion::input::MouseTerminal;
 use crate::ui::utils::event::{Event, Events};
@@ -13,13 +14,34 @@ use tui::style::{Color, Modifier, Style};
 //This module import is probably too verbose...
 use super::super::pokedex::lists;
 
-fn pokemon_names () -> Vec<String> {
+fn pokemon_names() -> Vec<String> {
     let map: HashMap<String, String> = lists::get_all_pokemon().unwrap();
     map.keys().cloned().collect()
 }
 
+struct Namelist {
+    names: Vec<String>,
+    selected: Option<usize>,
+}
+
+impl Namelist {
+    async fn new() -> Namelist {
+        Namelist {
+            names: pokemon_names(),
+            selected: None
+        }
+    }   
+}
+
+pub fn runner() -> Result<(), io::Error> {
+    println!("... Loading pokemon dictionary ...");
+    let name_list = block_on(Namelist::new());
+    draw_ui(name_list)?;
+    Ok(())
+}
+
 ///Handles drawing the ui.
-pub fn draw_ui() -> Result<(), io::Error> {
+fn draw_ui(mut name_list: Namelist) -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
     let stdout = MouseTerminal::from(stdout);
     let stdout = AlternateScreen::from(stdout);
@@ -28,7 +50,6 @@ pub fn draw_ui() -> Result<(), io::Error> {
     terminal.hide_cursor()?;
 
     let events = Events::new();
-    let names = pokemon_names();
 
     loop {
         terminal.draw(|mut f| {
@@ -43,9 +64,14 @@ pub fn draw_ui() -> Result<(), io::Error> {
                 )
                 .split(f.size());
             
+            let name_list_style = Style::default().fg(Color::Black).bg(Color::White);
             SelectableList::default()
                 .block(Block::default().borders(Borders::ALL).title("Pokemon"))
-                .items(&names)
+                .items(&name_list.names)
+                .select(name_list.selected)
+                .style(name_list_style)
+                .highlight_style(name_list_style.fg(Color::LightGreen).modifier(Modifier::BOLD))
+                .highlight_symbol(">")
                 .render(&mut f, chunks[0]);
 
             let text= [
@@ -62,6 +88,17 @@ pub fn draw_ui() -> Result<(), io::Error> {
             Event::Input(input) => match input {
                 Key::Char('q') => {
                     break;
+                }
+                Key::Down => {
+                    name_list.selected = if let Some(selected) = name_list.selected {
+                        if selected >= name_list.names.len() - 1 {
+                            Some(0)
+                        } else {
+                            Some(selected + 1)
+                        }
+                    } else {
+                        Some(0)
+                    }
                 }
                 _ => {}
             },
